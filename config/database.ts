aa479a640ffc104e3da98eab99a2c5c8f9b44069
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-01-04 23:12:37 +0300

    add: configured postgresql and ready to deploy on render

---
+++ config/database.ts
@@ -0,0 +1,30 @@
+export default ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapiblog"),
+        user: env("DATABASE_USERNAME", "db_user"),
+        password: env("DATABASE_PASSWORD", "password"),
+        ssl: false,
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
