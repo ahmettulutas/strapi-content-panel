02e0f79e822db0f4e72b3349a9966b67e8f22f9e
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-01-04 23:41:18 +0300

    fix: production database env variables fix

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -5,15 +5,18 @@ module.exports = ({ env }) => {
 
   return {
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-      },
-      debug: false,
+      connectionString: env("DATABASE_URL"),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapiblog"),
+      user: env("DATABASE_USERNAME", "db_user"),
+      password: env("DATABASE_PASSWORD", "password"),
+      ssl: false,
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
     },
   };
 };
