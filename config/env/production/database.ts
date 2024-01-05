cd5e23a03ebe6c79c983cf5b95fa5380384a0a4d
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-01-05 09:21:48 +0300

    bugfix: posgtre db integration

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,20 +1,19 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
   return {
     connection: {
-      connectionString: env("DATABASE_URL"),
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapiblog"),
-      user: env("DATABASE_USERNAME", "db_user"),
-      password: env("DATABASE_PASSWORD", "password"),
-      ssl: false,
-      schema: env("DATABASE_SCHEMA", "public"),
-    },
-    pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+      },
+      debug: false,
     },
   };
 };
